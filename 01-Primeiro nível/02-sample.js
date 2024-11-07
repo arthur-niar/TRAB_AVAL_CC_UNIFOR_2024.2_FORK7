17d0d76e42ad8e609f5230786608cadacd82825b
ALUNO K10 <alunok10@acad.unifor.br>
2024-11-07 20:19:08 -0300

    Acesso

--- 01-Primeiro nível/02-sample.js
+++ 01-Primeiro nível/02-sample.js
@@ -1,12 +1,10 @@
-let a = true;
-
-function verAc(u) {
-    if (a && u.isAdmin) {
+let acesso= true;
+function verAcesso(usuario) {
+    if (acesso && usuario.isAdmin) {
         console.log("Acesso permitido ao administrador.");
     } else {
         console.log("Acesso negado.");
     }
 }
-
-let u = { nome: "João", admin: true };
-verAc(u);
+let usuario = { nome: "João", admin: true };
+verAcesso(usuario);
